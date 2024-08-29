6d7c567e412ceba5150c57138c7496603167b6f6
Ann Connolly <biacatering4u@gmail.com>
2024-08-29 13:34:05 -0700

    first commmit

--- Labfiles/06-video-indexer/get-videos.ps1
+++ Labfiles/06-video-indexer/get-videos.ps1
@@ -1,5 +1,5 @@
-$account_id="YOUR_ACCOUNT_ID"
-$api_key="YOUR_API_KEY"
+$account_id="9742a68b-515a-436d-92df-87e12164c54f"
+$api_key="dc27851a663c415f910d114d4e41499d"
 $location="trial"
 
 # Call the AccessToken method with the API key in the header to get an access token
